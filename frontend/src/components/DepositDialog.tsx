1c1026b460d8e7212993151a64873bba2ca6ef22
Lokeshwaran Boopathy <lokeshwaran100@gmail.com>
2024-08-27 13:09:59 +0530

    Completed the flow for deposit with hardcoded values

--- frontend/src/components/DepositDialog.tsx
+++ frontend/src/components/DepositDialog.tsx
@@ -2,60 +2,29 @@ import React, { useState } from "react";
 import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {
-  Aptos,
-  Account,
-  Ed25519PrivateKey,
-  Serializer,
-  MoveVector,
-  U64,
-  Network,
-  AptosConfig
-} from "@aptos-labs/ts-sdk";
-import { panoraSwap } from "@/lib/apiRequests";
-
-const APTOS_COIN = "0x1::aptos_coin::AptosCoin";
-const wUSDC_TOKEN = "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T";
-const zUSDC_TOKEN = "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC";
+import { deposit, stakeWusdcZusdcPair, swapAptToWUsdc, swapAptToZUsdc } from "@/lib/apiRequests";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 const DepositDialogButton = () => {
+  const { account, signTransaction } = useWallet();
   const [amount, setAmount] = useState("");
 
   const handleConfirm = async () => {
-    // Add the logic for handling the deposit here
     console.log(`Depositing ${amount} USDT`);
 
-    const fromTokenAmount = "0.001"; // Amount of USDC to swap
-    const toWalletAddress = "0xfff0b85abd60c84d99ea725cede9d711276c09e2ec435a45777df0ca933b27cc";
-    const privateKey = process.env.NEXT_PUBLIC_ADMIN_PK;
+    const fromTokenAmount = "0.001";
+    const wUsdcSwapResponse = await swapAptToWUsdc(fromTokenAmount);
+    console.log("wUsdcSwapResponseresponse", wUsdcSwapResponse);
+    const zUsdcSwapResponse = await swapAptToZUsdc(fromTokenAmount);
+    console.log("zUsdcSwapResponse", zUsdcSwapResponse);
 
-    const wUsdcSwapResponse = await panoraSwap(APTOS_COIN, wUSDC_TOKEN, fromTokenAmount, toWalletAddress, privateKey);
-    console.log("response", wUsdcSwapResponse);
-    const zUsdcSwapResponse = await panoraSwap(APTOS_COIN, zUSDC_TOKEN, fromTokenAmount, toWalletAddress, privateKey);
-    console.log("response", zUsdcSwapResponse);
-
-    const config = new AptosConfig({ network: Network.MAINNET });
-    const aptos = new Aptos(config);
-    const privateKeyInstance = new Ed25519PrivateKey(
-      privateKey
-    );
-    const admin = Account.fromPrivateKey({ privateKey: privateKeyInstance });
-    const moduleAddress = "0x4bf51972879e3b95c4781a5cdcb9e1ee24ef483e7d22f2d903626f126df62bd1";
-    const wUSDC = BigInt(1 * 100000);
+    const wUSDC = BigInt(0.9 * 100000);
     const zUSDC = BigInt(1 * 100000);
+    const stakeResponse = await stakeWusdcZusdcPair(wUSDC, zUSDC);
+    console.log("stakeResponse", stakeResponse);
 
-    const transaction = await aptos.transaction.build.simple({
-      sender: admin.accountAddress,
-      data: {
-        function: `${moduleAddress}::router::add_liquidity_and_stake_both_coins_entry`,
-        typeArguments: [wUSDC_TOKEN, zUSDC_TOKEN],
-        functionArguments: [true, wUSDC, zUSDC],
-      },
-    });
-
-    const committedTxn = await aptos.signAndSubmitTransaction({ signer: admin, transaction: transaction });
-    await aptos.waitForTransaction({ transactionHash: committedTxn.hash });
-    console.log(`Committed transaction: ${committedTxn.hash}`);
+    const depositReponse = await deposit(account, amount, signTransaction);
+    console.log("depositReponse", depositReponse);
 
     setAmount("");
   };
--- frontend/src/lib/apiRequests.ts
+++ frontend/src/lib/apiRequests.ts
@@ -1,11 +1,45 @@
+import {
+    Aptos,
+    Account,
+    Ed25519PrivateKey,
+    Serializer,
+    MoveVector,
+    U64,
+    Network,
+    AptosConfig
+} from "@aptos-labs/ts-sdk";
 import Panora from "@panoraexchange/swap-sdk";
+
+const APTOS_COIN = "0x1::aptos_coin::AptosCoin";
+const wUSDC_TOKEN = "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T";
+const zUSDC_TOKEN = "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC";
+const toWalletAddress = "0xfff0b85abd60c84d99ea725cede9d711276c09e2ec435a45777df0ca933b27cc";
+
+const privateKey = process.env.NEXT_PUBLIC_ADMIN_PK as string;
+
+const aptos_mainnet = new Aptos(new AptosConfig({ network: Network.MAINNET }));
+const aptos_devnet = new Aptos(new AptosConfig({ network: Network.DEVNET }));
+const admin = Account.fromPrivateKey({
+    privateKey: new Ed25519PrivateKey(
+        privateKey
+    )
+});
+const cellanaAddress = "0x4bf51972879e3b95c4781a5cdcb9e1ee24ef483e7d22f2d903626f126df62bd1";
+const aptolizeAddress = "0xfff0b85abd60c84d99ea725cede9d711276c09e2ec435a45777df0ca933b27cc";
 
 // Initialize the Panora client
 const client = new Panora({
     apiKey: "oLujOsvnXgFY9TjN5VxS@u@kmq+wWjcyTEnVL4LEPf5pwNtYdR90EfeBDj33F^4E",
 });
 
-//@ts-ignore
+export async function swapAptToWUsdc(amount: string) {
+    return await panoraSwap(APTOS_COIN, wUSDC_TOKEN, amount, toWalletAddress, privateKey);
+}
+
+export async function swapAptToZUsdc(amount: string) {
+    return await panoraSwap(APTOS_COIN, zUSDC_TOKEN, amount, toWalletAddress, privateKey);
+}
+
 export async function panoraSwap(fromTokenAddress, toTokenAddress, fromTokenAmount, toWalletAddress, privateKey) {
     try {
         const response = await client.ExactInSwap(
@@ -34,4 +68,52 @@ export async function panoraSwap(fromTokenAddress, toTokenAddress, fromTokenAmou
         console.error("Error during swap:", error);
         throw error;
     }
+}
+
+export async function stakeWusdcZusdcPair(wUsdcAmount, zUsdcAmount) {
+    const transaction = await aptos_mainnet.transaction.build.simple({
+        sender: admin.accountAddress,
+        data: {
+            function: `${cellanaAddress}::router::add_liquidity_and_stake_both_coins_entry`,
+            typeArguments: [wUSDC_TOKEN, zUSDC_TOKEN],
+            functionArguments: [true, wUsdcAmount, zUsdcAmount],
+        },
+    });
+
+    const committedTxn = await aptos_mainnet.signAndSubmitTransaction(
+        {
+            signer: admin,
+            transaction: transaction
+        });
+    const response = await aptos_mainnet.waitForTransaction({ transactionHash: committedTxn.hash });
+    return response
+}
+
+export async function deposit(user, amount, signTransaction) {
+    console.log("user.address", user.address)
+    const transaction = await aptos_devnet.transaction.build.multiAgent({
+        sender: admin.accountAddress,
+        secondarySignerAddresses: [user.address],
+        data: {
+            function: `${aptolizeAddress}::aptolize::deposit`,
+            functionArguments: [amount],
+        },
+    });
+
+    const adminSenderAuthenticator = aptos_devnet.transaction.sign(
+        {
+            signer: admin,
+            transaction: transaction
+        });
+
+    const userSenderAuthenticator = await signTransaction(transaction);
+
+    const committedTxn = await aptos_devnet.transaction.submit.multiAgent({
+        transaction,
+        senderAuthenticator: adminSenderAuthenticator,
+        additionalSignersAuthenticators: [userSenderAuthenticator],
+    });
+
+    const response = await aptos_devnet.waitForTransaction({ transactionHash: committedTxn.hash });
+    return response
 }
